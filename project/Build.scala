bee1319595e7fae178dc051e42f4e70c6e75b6ae
Christopher Martin <ch.martin@gmail.com>
2013-03-19 00:49:23 -0400

    neo4j?

--- project/Build.scala
+++ project/Build.scala
@@ -4,8 +4,9 @@ import Keys._
 object Build extends sbt.Build {
 
   lazy val project = Project(id = "root", base = file(".")) settings (
-    scalaVersion := "2.10.0",
+    scalaVersion := "2.10.1",
     libraryDependencies ++= Seq(
+      "org.neo4j" % "neo4j-kernel" % "1.9.M05" classifier "tests",
       "org.scalatest" %% "scalatest" % "2.0.M5b"
     )
   )
--- src/test/scala/play.scala
+++ src/test/scala/play.scala
@@ -1,40 +1,71 @@
+import java.security.SecureRandom
+import org.neo4j.graphdb.factory.GraphDatabaseFactory
+import org.neo4j.kernel.EmbeddedGraphDatabase
+import org.neo4j.test.ImpermanentGraphDatabase
+import scala.util.Random
+
 trait PrivateAuthKey
 
 trait PublicAuthKey
 
-trait Assertion
+trait Statement
 
 trait Belief
 
 trait TrustSystem
 
-trait BitString {
+case class Anchor(bits: Seq[Byte])
 
-  def bits: Seq[Boolean]
-
-  def paddedBits(blockSize: Int): Seq[Boolean] = bits ++ Seq(true) ++
-    Stream.fill(BigInt(blockSize - bits.size - 1).mod(blockSize).toInt)(false)
-
-  sealed def asBytes: Seq[Byte] = ???
-
+trait AnchorGenerator {
+  def nextAnchor(): Anchor
 }
 
-trait Anchor {
+class RandomAnchors(random: Random = new SecureRandom()) extends AnchorGenerator {
 
-  def bits: BitString
-
+  def nextAnchor() = {
+    val array = new Array[Byte](20)
+    random.nextBytes(array)
+    Anchor(array)
+  }
 }
 
-trait TrustSystemSimulator
+class ConsecutiveAnchors(it: Iterator[Int] = Stream.from(0).iterator) extends AnchorGenerator {
+
+  def nextAnchor() = {
+    val array = new Array[Byte](4)
+    java.nio.ByteBuffer.wrap(array).putInt(it.next())
+    Anchor(array)
+  }
+}
+
+trait TrustSystemSimulator {
+
+  val graphDb: EmbeddedGraphDatabase = new ImpermanentGraphDatabase()
+
+  val anchorGenerator = new ConsecutiveAnchors()
+  def anchor: Anchor = anchorGenerator.nextAnchor()
+
+  implicit class RichAnchor(a: Anchor) {
+    def isA(b: Anchor): Statement = ???
+  }
+
+  def state(x: Any) = ???
+
+}
 
 import org.scalatest._
 
 object TrustSystemSimulatorSpec extends FreeSpec {
 
-  "" - {
+  """A very simple to-do list. Each entry has a string that describes it and
+    |a boolean flag indicating whether it is open or closed.
+  """.stripMargin -
+    new TrustSystemSimulator {
 
+      val todoListEntry = anchor
+      val firstEntry = anchor
+      state(firstEntry isA todoListEntry)
 
-
-  }
+    }
 
 }
